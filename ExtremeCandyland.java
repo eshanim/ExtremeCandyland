ed64e3c24b97ea48978c4b504188ae3e5f7112bc
eshanim <eshani.mehta@gmail.com>
2018-05-22 15:35:34 -0700

    Update ExtremeCandyland.java

--- ExtremeCandyland.java
+++ ExtremeCandyland.java
@@ -7,15 +7,21 @@ public class ExtremeCandyland
 
     private static Path p = new Path();
     private static DeckOfCards deck = new DeckOfCards();
-    public static PlayerList players = new PlayerList();
+    public static PlayerList players;
 
 
     public static void main( String[] args )
     {
         // TODO Auto-generated method stub
-        LoginWindow lw = new LoginWindow();
         
-        GameBoard gb = new GameBoard(players);
+        LoginWindow lw = new LoginWindow("Login");
+
+            lw.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+            lw.setBounds( 0, 0, 360, 500 );
+            lw.setVisible( true );
+        
+        players = lw.playerList;
+        Gameboard gb = new Gameboard(players);
         gb.showGridLayout();
         
         CardsWindow window = new CardsWindow(players); 
@@ -24,11 +30,12 @@ public class ExtremeCandyland
         window.setVisible( true );
         
         
-        
-        for (int i = players.getPosition(); i < players.getList().size(); i++)
+        int i = players.getPosition();
+        while(i < players.getList().size())
         {
             players.getList().get( i ).play( deck, p );
             players.setPosition( i + 1 );
+            gb.changePlayer();
             
             if (i == players.getList().size() - 1)
             {
