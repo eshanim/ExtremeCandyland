90cca0d09fd0063d32c1b4c41f32fed7df1b9140
eshanim <eshani.mehta@gmail.com>
2018-05-22 15:36:20 -0700

    Update LoginWindow.java

--- LoginWindow.java
+++ LoginWindow.java
@@ -19,8 +19,9 @@ public class LoginWindow extends JFrame
   private JTextField nameField5;
   private JTextField nameField6;
   
-  public PlayerList playerList;
+  public static PlayerList playerList = new PlayerList();
 
+  
 
   /**
    *  Constructs a new login window.
@@ -35,32 +36,38 @@ public class LoginWindow extends JFrame
 
     JLabel instructions = new JLabel("You need at least two players.");
     
-    JLabel name1 = new JLabel("RED PLAYER:", JLabel.RIGHT);
+    JLabel name1 = new JLabel("RED PLAYER:", JLabel.LEFT);
     nameField = new JTextField(20);
 
     LoginListener loginListener = new LoginListener();
-    JLabel name2 = new JLabel("ORANGE PLAYER:", JLabel.RIGHT);
+    JLabel name2 = new JLabel("ORANGE PLAYER:", JLabel.LEFT);
     nameField2 = new JTextField(20);
-    JLabel name3 = new JLabel("YELLOW PLAYER:", JLabel.RIGHT);
+    JLabel name3 = new JLabel("YELLOW PLAYER:", JLabel.LEFT);
     nameField3 = new JTextField(20);
-    JLabel name4 = new JLabel("GREEN PLAYER:", JLabel.RIGHT);
+    JLabel name4 = new JLabel("GREEN PLAYER:", JLabel.LEFT);
     nameField4 = new JTextField(20);
-    JLabel name5 = new JLabel("BLUE PLAYER:", JLabel.RIGHT);
+    JLabel name5 = new JLabel("BLUE PLAYER:", JLabel.LEFT);
     nameField5 = new JTextField(20);
-    JLabel name6 = new JLabel("PURPLE PLAYER:", JLabel.RIGHT);
+    JLabel name6 = new JLabel("PURPLE PLAYER:", JLabel.LEFT);
     nameField6 = new JTextField(20);
 
     JButton playBtn = new JButton("Play");
     playBtn.addActionListener(loginListener);
 
-    JPanel fieldsPanel = new JPanel(new GridLayout(3, 3, 10, 10));
+    JPanel fieldsPanel = new JPanel(new GridLayout(0, 1, 5, 5));
     fieldsPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
 
+    fieldsPanel.add( name1 );
     fieldsPanel.add(nameField);
+    fieldsPanel.add( name2 );
     fieldsPanel.add(nameField2);
+    fieldsPanel.add( name3 );
     fieldsPanel.add(nameField3);
+    fieldsPanel.add( name4 );
     fieldsPanel.add(nameField4);
+    fieldsPanel.add( name5 );
     fieldsPanel.add(nameField5);
+    fieldsPanel.add( name6 );
     fieldsPanel.add(nameField6);
     fieldsPanel.add(new JPanel());  // filler
 
@@ -90,35 +97,34 @@ public class LoginWindow extends JFrame
       if (!name1.equals( "" ))
       {
           Player p1 = new Player(name1, Color.RED);
-          players.add( p1 );
+          playerList.add( p1 );
       }
       if (!name2.equals( "" ))
       {
           Player p2 = new Player(name2, Color.ORANGE);
-          players.add( p2 );
+          playerList.add( p2 );
       }
       if (!name3.equals( "" ))
       {
           Player p3 = new Player(name3, Color.YELLOW);
-          players.add( p3 );
+          playerList.add( p3 );
       }
       if (!name4.equals( "" ))
       {
           Player p4 = new Player(name4, Color.GREEN);
-          players.add( p4 );
+          playerList.add( p4 );
       }
       if (!name5.equals( "" ))
       {
           Player p5 = new Player(name5, Color.BLUE);
-          players.add( p5 );
+          playerList.add( p5 );
       }
       if (!name6.equals( "" ))
       {
           Player p6 = new Player(name6, Color.MAGENTA);
-          players.add( p6 );
+          playerList.add( p6 );
       }
-      
-      playerList = new PlayerList(players);
-    }
+
+      }
   }
 }
