29225adc2d53f4dceefcd06c6d004120b175273b
juhithomas <38888073+juhithomas@users.noreply.github.com>
2018-05-22 14:23:19 -0700

    Update ExtremeCandyland.java

--- ExtremeCandyland.java
+++ ExtremeCandyland.java
@@ -1,38 +1,41 @@
-import java.awt.Color;
+import java.util.*;
 
 import javax.swing.JFrame;
-import java.util.*;
-import info.gridworld.grid.Location;
 
 public class ExtremeCandyland
 {
-    Path p;
-    
+
+    private static Path p = new Path();
+    private static DeckOfCards deck = new DeckOfCards();
+    public static PlayerList players = new PlayerList();
+
+
     public static void main( String[] args )
     {
         // TODO Auto-generated method stub
+        LoginWindow lw = new LoginWindow();
+        
+        GameBoard gb = new GameBoard(players);
+        gb.showGridLayout();
+        
+        CardsWindow window = new CardsWindow(players); 
+        window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        window.setBounds( 0, 0, 187, 290 );
+        window.setVisible( true );
         
         
-//        Users server = new Users();
-//        LoginWindow window = new LoginWindow( "Extreme Candyland", server );
-//        window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-//        window.setBounds( 0, 0, 280, 140 );
-//        window.setVisible( true );
-//        
-//        Path p = new Path();
-//        p.getList();
-//        System.out.println(p.getList().size());
         
-//        DeckOfCards doc = new DeckOfCards();
-//        Card c = doc.getList().get( 0 );
-//        doc.shuffle();
-//        List<Card> b = doc.getList();
-//        System.out.println(c);
-//        System.out.println(b);
+        for (int i = players.getPosition(); i < players.getList().size(); i++)
+        {
+            players.getList().get( i ).play( deck, p );
+            players.setPosition( i + 1 );
+            
+            if (i == players.getList().size() - 1)
+            {
+                players.setPosition(0);
+            }
+        }
         
-        Path p = new Path();
-        ColorTile t = p.findNextColorTile( new Location(14,0), Color.GREEN );
-        System.out.println(t);
     }
 
 }
